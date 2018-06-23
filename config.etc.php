149713f15469d89c7bf39b87e9ace3c75471e76c
RazeSoldier <razesoldier@outlook.com>
2018-06-23 14:23:01 +0800

    Add a sample configuration file

---
+++ config.etc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Sample configuration
+ */
+
+# Backup config
+$gBackupType = ''; // Required, backup type (Allow value: 'logical' or 'physical')
+// If $gBackupType set to 'logical', please configure @{
+$gDBType = ''; // The database software type (Allow value: 'mysql')
+$gDBHost = ''; // The database address
+$gDBUsername = ''; // The database username that used to backup
+$gDBPassword = ''; // The database password that used to backup
+// The databases you want to back up
+// Allow value: a single database name, an array that includes multiple database names or 'all' (backup all databases)
+$gDBWantDump = '';
+// @}
+
+# Storage config
+$gStorageType = ''; // Required, storage type (Allow value: 'local' or 'remote')
+// If $gStorageType set to 'remote', please configure @{
+$gRemoteType = ''; // Remote type (Allow value: 'qcloud')
+# ... Other remote configuration
+// @}
